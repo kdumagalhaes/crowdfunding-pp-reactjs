c092a0ed2f3e106422d9a5da8d90409851b1a44a
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-12-04 14:23:53 -0300

    Add a tag in logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,23 @@
-import { Container, Navigation } from './styles'
+import { Container, Content, Navigation } from './styles'
 
 import logoImg from '../../assets/img/logo.svg'
 
 export const Header = () => {
     return (
         <Container>
-            <img src={logoImg} alt="crowdfund" />
-            <Navigation>
-                <ul>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Discover</a></li>
-                    <li><a href="#">Get Started</a></li>
-                </ul>
-            </Navigation>
+            <Content>
+                <a href="#">
+                    <h1 className="seo-h1">Crowdfunding • This is just for SEO best practices =)</h1>
+                    <img src={logoImg} alt="crowdfund" />
+                </a>
+                <Navigation>
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Discover</a></li>
+                        <li><a href="#">Get Started</a></li>
+                    </ul>
+                </Navigation>
+            </Content>
         </Container>
     )
 }
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,15 +1,30 @@
 import styled from 'styled-components'
 
-export const Container = styled.header `
+import desktopImgBg from '../../assets/img/image-hero-desktop.jpg'
+
+
+export const Container = styled.header`
     width: 100%;
-    max-width: 1190px;
-    padding: var(--safe-area-padding);
-    margin: 50px auto;
-    display: flex;
-    justify-content: space-between;
+    height: 400px;
+    background: url(${desktopImgBg}) no-repeat left top;
+    
 `
 
-export const Navigation = styled.nav `    
+export const Content = styled.div`
+    width: 100%;
+    max-width: 1190px;
+    padding: 50px 40px 0 40px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+
+    .seo-h1 {
+        font-size: 1px;
+        visibility: hidden;
+    }
+`
+
+export const Navigation = styled.nav`    
     ul {
         width: 100%;
         max-width: 500px;
@@ -17,6 +32,7 @@ export const Navigation = styled.nav `
 
         li {
             margin-left: 50px;
+            list-style: none;
         }
     }
 `
