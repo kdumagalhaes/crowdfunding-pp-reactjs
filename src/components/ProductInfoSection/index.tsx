bbaed4b465d8d08282c730e93858bfe248a5e1f3
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-12-05 12:30:38 -0300

    Add buttons in product info section

--- src/components/ProductInfoSection/index.tsx
+++ src/components/ProductInfoSection/index.tsx
@@ -1,6 +1,8 @@
 import { Container, Content } from "./styles"
 
 import mastercraftLogo from '../../assets/img/logo-mastercraft.svg'
+import bookmarkIcon from '../../assets/img/icon-bookmark.svg'
+
 import { PrimaryButton } from "../PrimaryButton"
 
 export const ProductInfoSection = () => {
@@ -10,7 +12,13 @@ export const ProductInfoSection = () => {
                 <img className="product-logo" src={mastercraftLogo} alt="logo mastercraft" />
                 <h1 className="product-name">Mastercraft Bamboo Monitor Riser</h1>
                 <p className="product-summary">A beautiful & handcrafted monitor stand to reduce neck and eye strain.</p>
-                <PrimaryButton text="Back this project" />
+                <div className="flex-container">
+                    <PrimaryButton text="Back this project" />
+                    <button className="btn-bookmark">
+                        <img src={bookmarkIcon} alt="bookmark icon" />
+                        <p>Bookmark</p>
+                    </button>
+                </div>
             </Content>
         </Container>
     )
--- src/components/ProductInfoSection/styles.tsx
+++ src/components/ProductInfoSection/styles.tsx
@@ -4,10 +4,9 @@ export const Container = styled.section`
     padding: var(--safe-area-padding);
 `
 
-export const Content = styled.section `
+export const Content = styled.section`
     width: 100%;
     max-width: 730px;
-    height: 200px;
     background: #fff;
     border-radius: 9px;
     border: 1px solid #eee;
@@ -37,5 +36,25 @@ export const Content = styled.section `
     .product-name,
     .product-summary {
         text-align: center;
+    }
+
+    .flex-container {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 40px;
+    }
+
+    .btn-bookmark {
+        border-radius: 50px;
+        border: none;
+        display: flex;
+        align-items: center;
+
+        p {
+          font-weight: 700;
+          color: var(--dark-gray);
+          font-size: 1rem;
+          padding: 0 30px 0 20px;
+        }
     }
 `
