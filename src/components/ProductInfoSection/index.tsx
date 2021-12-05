491c60b5f24cc5dd046f0706bf79b22af3c45bbc
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-12-05 13:19:42 -0300

    Finish product info section

--- src/components/ProductInfoSection/index.tsx
+++ src/components/ProductInfoSection/index.tsx
@@ -4,8 +4,12 @@ import mastercraftLogo from '../../assets/img/logo-mastercraft.svg'
 import bookmarkIcon from '../../assets/img/icon-bookmark.svg'
 
 import { PrimaryButton } from "../PrimaryButton"
+import { useState } from "react"
 
 export const ProductInfoSection = () => {
+
+    const [bookmark, setBookmark] = useState(true)
+
     return (
         <Container>
             <Content>
@@ -14,9 +18,16 @@ export const ProductInfoSection = () => {
                 <p className="product-summary">A beautiful & handcrafted monitor stand to reduce neck and eye strain.</p>
                 <div className="flex-container">
                     <PrimaryButton text="Back this project" />
-                    <button className="btn-bookmark">
+                    <button 
+                    style={bookmark ? {} : { background: 'hsl(176, 50%, 97%)', color: 'red' }} 
+                    onClick={() => setBookmark(!bookmark)} 
+                    className="btn-bookmark"
+                    >
                         <img src={bookmarkIcon} alt="bookmark icon" />
-                        <p>Bookmark</p>
+                        {bookmark ?
+                            <p>Bookmark</p>
+                            :
+                            <p style={{color: 'var(--dark-cyan)'}}>Bookmarked</p>}
                     </button>
                 </div>
             </Content>
