4ee158fd3983c8ac964f497230152c128d211597
Carlos Pereira <ce.magalhaespereira@gmail.com>
2022-01-16 17:07:07 -0300

    Add snapshot test on Head component

---
+++ src/components/Header/__tests__/index.test.jsx
@@ -0,0 +1,9 @@
+import {render, screen} from "@testing-library/react"
+import {Header} from ".."
+
+describe("Header", () => {
+    it("initial component should match snapshot", () => {
+        const {asFragment} = render(<Header />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
